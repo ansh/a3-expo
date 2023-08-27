0ebc8f692017b3b0b58a75e7f902d38d6f8a9abd
Ansh Nanda <anshnanda10@gmail.com>
2023-08-27 14:03:48 -0700

    add `platform.ts` helpers

---
+++ src/lib/platform.ts
@@ -0,0 +1,8 @@
+import { Platform } from "react-native";
+import Constants, { ExecutionEnvironment } from "expo-constants";
+
+// device platform
+export const isIOS = Platform.OS === "ios";
+export const isAndroid = Platform.OS === "android";
+export const isNative = isIOS || isAndroid;
+export const isExpoGo = Constants.executionEnvironment === ExecutionEnvironment.StoreClient; // use this to check if the app is running in Expo Go, and add fallbacks for features that aren't available in Expo Go.
